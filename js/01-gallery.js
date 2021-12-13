aafc1118e22e75a5bfbf3206bcdcc6800b0cebc3
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2021-12-13 09:18:14 +0100

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
+'use strict'
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const instance = basicLightbox.create(
+`<img src="" />`
+ );
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    modal: instance.element().querySelector('img'),
+};
+
+const galleryMarkup = createGalleryMarkup();
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg" onclick="event.preventDefault()">
+                <img
+                class="gallery__image"
+                src='${preview}'
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>`;
+    })
+    .join('');
+};
+
+refs.gallery.addEventListener('click', onOpenModal);
+refs.gallery.addEventListener('keydown', onCloseModal);
+
+function onOpenModal(e) { 
+ e.preventDefault() 
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  refs.modal.src = e.target.dataset.source;
+  instance.show()
+};
+
+function onCloseModal() {
+window.removeEventListener('keydown', onEscKeyPress);
+  refs.modal.src = '';
+  instance.close()
+};
+
+function onEscKeyPress(e) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = e.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+        onCloseModal();
+    }
+};  
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
+'use strict'
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+
+const markupGallery = createGalleryMarkup(galleryItems);
+
+container.insertAdjacentHTML("beforeend", markupGallery);
+
+function createGalleryMarkup (gallery) {
+   return gallery
+    .map(({preview, original, description}) => { 
+      return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>`;
+    })
+    .join(""); 
+};
+
+new SimpleLightbox('.gallery__item', {captionsData:'alt', captionDelay:'250', captionType: 'alt', scrollZoomFactor: 0.5, captionPosition: 'bottom',});   
